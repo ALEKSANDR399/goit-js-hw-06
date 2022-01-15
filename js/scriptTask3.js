9b9e6deb83fcded4ec1b35f573289cf0f57be8ab
ALEKSANDR399 <88842048+ALEKSANDR399@users.noreply.github.com>
2022-01-15 13:50:34 +0200

    ДЗ№6

--- js/scriptTask3.js
+++ js/scriptTask3.js
@@ -1,26 +1,26 @@
-// const images = [
-//   {
-//     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260',
-//     alt: 'White and Black Long Fur Cat',
-//   },
-//   {
-//     url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260',
-//     alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
-//   },
-//   {
-//     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260',
-//     alt: 'Group of Horses Running',
-//   },
-// ];
+const images = [
+  {
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+];
 
-// const galleryList = document.querySelector('.gallery');
+const galleryList = document.querySelector('.gallery');
 
-// const listImg = [];
+const listImg = [];
    
-//     images.map(({url, alt}) => {
-//         const itemCreate = document.createElement('li');
-//         itemCreate.insertAdjacentHTML("afterbegin", `<img src = ${url} alt ='${alt}' width = 640 height = 320>`);
-//         return listImg.push(itemCreate);
-//     });
+    images.map(({url, alt}) => {
+        const itemCreate = document.createElement('li');
+        itemCreate.insertAdjacentHTML("afterbegin", `<img src = ${url} alt ='${alt}' width = 640 height = 320>`);
+        return listImg.push(itemCreate);
+    });
 
-// const listWithImg = galleryList.append(...listImg);
+const listWithImg = galleryList.append(...listImg);
