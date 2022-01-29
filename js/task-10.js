e4db04c972ad30aab1196e36ea83e0c4a5b38c2b
ALEKSANDR399 <88842048+ALEKSANDR399@users.noreply.github.com>
2022-01-29 17:43:46 +0200

    ДЗ№8

--- js/task-10.js
+++ js/task-10.js
@@ -1,42 +1,34 @@
-const controlsBox = document.querySelector('#controls');
-const windowEnterCoins = document.querySelector('input');
-const btnAddBox = document.querySelector('button[data-create]');
-const btnRemoveBox = document.querySelector('button[data-destroy]');
-const boxForCreateBoxes = document.querySelector('#boxes');
+const controlsBox = document.querySelector("#controls");
+const windowEnterCoins = document.querySelector("input");
+const btnAddBox = document.querySelector("button[data-create]");
+const btnRemoveBox = document.querySelector("button[data-destroy]");
+const boxForCreateBoxes = document.querySelector("#boxes");
 
-
-
-btnAddBox.addEventListener('click', () => { 
-    createBoxes(windowEnterCoins.value);
-   
+btnAddBox.addEventListener("click", () => {
+  createBoxes(windowEnterCoins.value);
 });
-btnRemoveBox.addEventListener('click', clearValueList);
+btnRemoveBox.addEventListener("click", clearValueList);
 
-function clearValueList ()  {
-    windowEnterCoins.value = '';
-    boxForCreateBoxes.innerHTML = '';
-};
+function clearValueList() {
+  windowEnterCoins.value = "";
+  boxForCreateBoxes.innerHTML = "";
+}
 
 function createBoxes(amount) {
-    
-    let sizeBox = 0;
-    let groupOfBox = [];
-    for (let i = 0; i < amount; i += 1) {
-      const createBox = document.createElement('div');
-      sizeBox = 30 + i * 10;
-      createBox.textContent = 'ubsbvbdknksn'
-      createBox.style.width = `${sizeBox}`;
-      createBox.style.height = `${sizeBox}`;
-      createBox.style.backgroundColor = getRandomHexColor();
-      groupOfBox.push(createBox);
-  };
+  let sizeBox = 0;
+  let groupOfBox = [];
+  for (let i = 0; i < amount; i += 1) {
+    const createBox = document.createElement("div");
+    sizeBox = 30 + i * 10;
+    createBox.style.width = `${sizeBox}`;
+    createBox.style.height = `${sizeBox}`;
+    createBox.style.backgroundColor = getRandomHexColor();
+    groupOfBox.push(createBox);
+  }
 
   return boxForCreateBoxes.append(...groupOfBox);
-   
-};
- 
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-   
-};
-
+}
